56626bf92a6ccca4d71b608cce812beadd393e0f
Stackbit Code Editor <projects@stackbit.com>
2021-04-08 12:43:04 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -69,6 +69,9 @@ export default class HeroSection extends React.Component {
                                 )}
                                 {_.get(this.props, 'data.config.footer.content', null) && (
                 						<span>{htmlToReact(_.get(this.props, 'data.config.footer.content', null))}</span>
+                                    )}
+                                {_.get(this.props, 'data.config.footer.content', null) && (
+                						<span>{htmlToReact(_.get(this.props, 'data.config.footer.content', null))}</span>
                 					)}
                 				{_.get(section, 'actions', null) && (
                 				<div className={classNames('hero__actions', 'btn-group', {'justify-center': align_x === 'center', 'justify-end': align_x === 'right'})}>
